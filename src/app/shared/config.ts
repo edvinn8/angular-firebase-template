ef4001077f696cfb44d11802d2921811e6e6f6f2
Edvin <edvinnadj@gmail.com>
2020-04-14 01:36:03 +0200

    Add firebase config to activate Live Demo page

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,13 +1,13 @@
 export const settings = {
   apiConfig: {
     // replace this with the config from Google Firebase console
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: ''
+    apiKey: 'AIzaSyA5Ch2ARjTvCr5mZJlItLxPBFhnZdHbTOM',
+    authDomain: 'angular-firebase-templat-83a37.firebaseapp.com',
+    databaseURL: 'https://angular-firebase-templat-83a37.firebaseio.com',
+    projectId: 'angular-firebase-templat-83a37',
+    storageBucket: 'angular-firebase-templat-83a37.appspot.com',
+    messagingSenderId: '387760501087',
+    appId: '1:387760501087:web:1ad24c88f8c0a6785006ea'
   },
   uiConfig: {
     sidebar: {
